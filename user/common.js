81ce01815c80eb821cc823bf5c17dc150cd0a6d8
Zzyyywo <414508640@qq.com>
2020-08-20 16:25:18 +0800

    14-完成在主分支master中修改。

--- user/common.js
+++ user/common.js
@@ -1,5 +1,12 @@
 const $ = {
     oName = function(oer) {
-        console.log(oer.Math.random());
+        if (oer) {
+            console.log('原始语句');
+        };
+    },
+    oGender = (per) => {
+        if (per > 20 && per < 30) {
+            console.log('这是在master分支中创造出的分支。');
+        };
     },
 };
