c0611bf94afebae171f8e70587cb09adc993915b
Zzyyywo <414508640@qq.com>
2020-08-20 16:23:01 +0800

    13-完成在dee-login分支中修改。

--- user/common.js
+++ user/common.js
@@ -1,5 +1,12 @@
 const $ = {
     oName = function(oer) {
-        console.log(oer.Math.random());
+        if (oer) {
+            console.log('原始代码');
+        };
+    },
+    oage = function(per) {
+        if (per) {
+            console.log('我是在dee-login分支创建出来的方法。');
+        };
     },
 };
